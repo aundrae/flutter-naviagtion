f47bab2573bd2978e527d6987fef69bffa108bf1
aundrae <justinramjattan@outlook.com>
2020-06-29 17:31:34 -0400

    addeding testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 //Replace with ./components/drawer.dart
 import './components/bottom_navigation.dart';
+import './screens/testing.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: MyHomePage(title: "App"),
-    );
+    return TodoList();
   }
 }
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,15 +7,56 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:newapp/main.dart';
-import 'package:newapp/screens/intent.dart';
+import 'package:newapp/screens/testing.dart';
 void main() {
 
-  testWidgets("Finds title", (WidgetTester tester) async{
-    await tester.pumpWidget(IntentScreen(text: 'screen'));
-    final title = find.text('screen');
+  //Test #1
+  testWidgets('Check if TextField is functional', (WidgetTester tester) async {
+    // Build the widget
+    await tester.pumpWidget(TodoList());
 
-    expect(title, findsWidgets);
+    // Enter 'hi' into the TextField.
+    await tester.enterText(find.byType(TextField), 'hi');
+  });
+
+  //Test #2
+  testWidgets('Enter text in TextField and add it to ListView', (WidgetTester tester) async {
+    // Enter text code...
+    await tester.pumpWidget(TodoList());
+
+    // Enter 'hi' into the TextField.
+    await tester.enterText(find.byType(TextField), 'hi');
+    // Tap the add button.
+    await tester.tap(find.byType(FloatingActionButton));
+
+    // Rebuild the widget after the state has changed.
+    await tester.pump();
+
+    // Expect to find the item on screen.
+    expect(find.text('hi'), findsOneWidget);
+  });
+
+  //Test#3
+  testWidgets('Test swipe to remove item from ListView', (WidgetTester tester) async {
+    // Enter text and add the item...
+    await tester.pumpWidget(TodoList());
+
+    // Enter 'hi' into the TextField.
+    await tester.enterText(find.byType(TextField), 'hi');
+
+    // Tap the add button.
+    await tester.tap(find.byType(FloatingActionButton));
+
+    //Rebuild Screen
+    await tester.pump();
+
+    // Swipe the item to dismiss it.
+    await tester.drag(find.byType(Dismissible), Offset(500.0, 0.0));
+
+    // Build the widget until the dismiss animation ends.
+    await tester.pumpAndSettle();
+
+    // Ensure that the item is no longer on screen.
+    expect(find.text('hi'), findsNothing);
   });
 }
